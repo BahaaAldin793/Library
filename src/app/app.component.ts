2caac54be9851bc311ed4423ec3eb5c40376e1bc
BahaaAldin793 <bdalnaser069@gmail.com>
2024-10-11 23:41:50 +0300

    login and registeration addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogInComponent } from "./components/log-in/log-in.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogInComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
